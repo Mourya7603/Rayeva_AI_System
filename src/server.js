59a30f5def5ff3636db532e3b802c3e922ef1443
Mangalapalli Mourya <magalapallimourya@gmail.com>
2026-03-05 15:17:51 +0530

    fix cors

--- src/server.js
+++ src/server.js
@@ -15,8 +15,16 @@ const PORT = process.env.PORT || 3000;
 
 // Security middleware
 app.use(helmet());
-app.use(cors());
+app.use(cors({
+  origin: [
+    "https://rayeva-ai-system-frontend.vercel.app",
+    "http://localhost:3000"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 
+app.options('*', cors());
 // Rate limiting
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
